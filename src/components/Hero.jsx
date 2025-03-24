0e918c1c95069b43fb6d8dc170398f01fc978e4f
mohit <montyits71@gmail.com>
2025-03-24 12:09:49 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,94 @@
+"use client";
+import { PIE_LIST } from "@/utils/helper";
+import React, { useEffect, useRef } from "react";
+import CustomHero from "./common/CustomHero";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Hero = () => {
+  const heroRef = useRef(null);
+
+  useEffect(() => {
+    const sections = heroRef.current?.querySelectorAll(".hero-section");
+    gsap.set(sections[0], { opacity: 1, y: 0 });
+    gsap.set(sections, { opacity: 0, y: 50 }); 
+    gsap.set(sections[0], { opacity: 1, y: 0 }); 
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: heroRef.current,
+        start: "top top",
+        end: () => `+=${(sections.length - 1) * 100}%`,
+        scrub: 1,
+        pin: true,
+        anticipatePin: 1,
+      },
+    });
+
+    sections.forEach((section, index) => {
+      if (index > 0) {
+        tl.to(section, {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: "power3.out",
+        });
+      }
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+    };
+  }, []);
+
+  return (
+    <div
+      ref={heroRef}
+      className="flex items-center  justify-center min-h-screen w-full"
+    >
+      <div className="flex flex-col items-center w-full max-w-[1140px] mx-auto">
+        {PIE_LIST.map((item, index) => (
+          <div className="hero-section relative w-full" key={index}>
+            <CustomHero
+              idClass={item.id}
+              bgClass={`${
+                index === 1
+                  ? "bg-[#6621E9]"
+                  : index === 2
+                  ? "bg-[#FFABC7]"
+                  : index === 3
+                  ? "bg-[#026070]"
+                  : "bg-[#6FFFA8]"
+              }`}
+              headingText={item.heading}
+              headingStyle={`${
+                index === 3
+                  ? "text-white"
+                  : index === 1
+                  ? "text-white"
+                  : "text-black"
+              }`}
+              descriptionOneStyle={`${
+                index === 3
+                  ? "text-white"
+                  : index === 1
+                  ? "text-white"
+                  : "text-black"
+              }`}
+              descriptionOneText={item.descriptionOne}
+              descriptionTwoText={item.descriptionTwo}
+              descriptionThreeText={item.descriptionThree}
+              descriptionFourText={item.descriptionFour}
+              headingTextTwo={item.headingTwo}
+              descriptionFiveText={item.descriptionFive}
+              Img={item.mobileImg}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/common/CustomHero.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import Image from 'next/image'
+const CustomHero = ({
+  bgClass,
+  headingText,
+  headingStyle,
+  descriptionOneText,
+  descriptionOneStyle,
+  headingTextTwo,
+  descriptionFourText,
+  descriptionThreeText,
+  descriptionTwoText,
+  descriptionFiveText,
+  Img,
+  mainClass,
+  idClass,
+}) => {
+  return (
+    <>
+      <div
+        id={idClass}
+        className={`flex max-md:flex-wrap px-4 absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] items-center gap-4 w-full max-w-[1140px] mx-auto ${mainClass}`}
+      >
+        <div
+          className={`${bgClass} w-full max-w-[525px] rounded-[16px] max-md:max-w-full  h-[675px] max-xl:h-full p-5`}
+        >
+          <h1
+            className={`${headingStyle} font-semibold  text-[26px] leading-[130%]`}
+          >
+            {headingText}
+          </h1>
+          <p
+            className={`${descriptionOneStyle} font-normal mt-[26px] text-[16px]   leading-[150%]`}
+          >
+            {descriptionOneText}
+          </p>
+          <p
+            className={`${descriptionOneStyle} font-normal text-[16px] mt-2   leading-[150%]`}
+          >
+            {descriptionTwoText}
+          </p>
+          <p
+            className={`${descriptionOneStyle} font-normal mt-2 text-[16px]   leading-[150%]`}
+          >
+            {descriptionThreeText}
+          </p>
+          <p
+            className={`${descriptionOneStyle} font-normal mt-2 text-[16px]   leading-[150%]`}
+          >
+            {descriptionFourText}
+          </p>
+          <h2
+            className={`${headingStyle} font-semibold text-[26px] mt-[26px]   leading-[130%] `}
+          >
+            {headingTextTwo}
+          </h2>
+          <p
+            className={`font-normal   mt-[26px] max-md:mt-2 text-[16px] leading-[150%] ${descriptionOneStyle}`}
+          >
+            {descriptionFiveText}
+          </p>
+        </div>
+        <div className={`w-full`}>
+          <Image
+            src={Img}
+            alt="img"
+            width={604}
+            height={675}
+            className="w-full max-w-[604px] h-[675px] max-xl:h-full"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CustomHero
